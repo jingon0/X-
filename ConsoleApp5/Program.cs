4676ea2ea09d53177abacf0aa9c1f97bdf87277c
jingon0 <168407337+jingon0@users.noreply.github.com>
2024-05-27 13:24:35 +0900

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static int ReadData()
+    {
+        Console.Write("Enter the size of the X: ");
+        string input = Console.ReadLine();
+        int size;
+        while (!int.TryParse(input, out size) || size <= 0)
+        {
+            Console.Write("Invalid input. Please enter a positive integer: ");
+            input = Console.ReadLine();
+        }
+        return size;
+    }
+
+    static void print_x_shape(int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                if (i == j || i + j == size - 1)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Main()
+    {
+        int size = ReadData();
+        print_x_shape(size);
+    }
+}
